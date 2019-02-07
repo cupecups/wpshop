55b435d67cabc1e85fdac48e3e3b225a06d3c14a
Yusuf Arifianto <cupecups@gmail.com>
2019-02-07 10:24:51 +0700

    Add files via upload

---
+++ viewer/story.js
@@ -0,0 +1,35 @@
+var story = {
+"docName": "ecomwp",
+"docPath": "P_P_P",
+"docVersion": "V_V_V",
+"hasRetina": true,
+"disableHotspots": false,
+"pages": [
+$.extend(new ViewerPage(),{
+"index": 0,
+"image": "shop_home.png",
+"image2x": "shop_home@2x.png",
+"width": 1920,
+"height": 4321,
+"title": "shop home",
+'type': 'regular',
+'fixedPanels': [],
+'links' : [],
+})
+,$.extend(new ViewerPage(),{
+"index": 1,
+"image": "shop_item.png",
+"image2x": "shop_item@2x.png",
+"width": 1920,
+"height": 4354,
+"title": "shop item",
+'type': 'regular',
+'fixedPanels': [],
+'links' : [],
+})
+   ]
+,"resolutions": [2],
+"title": "ECOMWP",
+"totalImages": 2,
+"highlightLinks": false
+}
